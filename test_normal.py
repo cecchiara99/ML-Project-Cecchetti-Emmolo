bc41667e90751a832a9b6f39f9c6c7fbd541e9ba
cecchiara99 <c.cecchetti@studenti.unipi.it>
2023-12-28 19:52:26 +0100

    modified:   test_normal.py

--- test_normal.py
+++ test_normal.py
@@ -42,9 +42,34 @@ all_values_2 = [value for sublist in data_2 for value in sublist]
 all_values_3 = [value for sublist in data_3 for value in sublist]
 
 # Perform the Shapiro-Wilk test for normality on the combined data
+significance_level = 0.05
 stat_1, p_value_1 = stats.shapiro(all_values_1)
+
+
+print(f"Shapiro-Wilk test statistic: {stat_1}, p-value: {p_value_1}")
+
+
+if p_value_1 > significance_level:
+    print("Globalmente, i dati sembrano seguire una distribuzione normale (MONK1).")
+else:
+    print("Globalmente, i dati non sembrano seguire una distribuzione normale (MONK1).\n")
+
 stat_2, p_value_2 = stats.shapiro(all_values_2)
+print(f"Shapiro-Wilk test statistic: {stat_2}, p-value: {p_value_2}")
+
+if p_value_2 > significance_level:
+    print("Globalmente, i dati sembrano seguire una distribuzione normale (MONK2).")
+else:
+    print("Globalmente, i dati non sembrano seguire una distribuzione normale (MONK2).\n")
+
+
 stat_3, p_value_3 = stats.shapiro(all_values_3)
+print(f"Shapiro-Wilk test statistic: {stat_3}, p-value: {p_value_3}")
+if p_value_3 > significance_level:
+    print("Globalmente, i dati sembrano seguire una distribuzione normale (MONK3).")
+else:
+    print("Globalmente, i dati non sembrano seguire una distribuzione normale (MONK3).\n")
+
 
 # Function to determine whether to reject the null hypothesis for Anderson-Darling test
 def reject_null_hypothesis_anderson(test_statistic, critical_values, significance_levels):
@@ -60,54 +85,66 @@ print(f"\nAnderson-Darling test statistic 1: {result_1.statistic}, critical valu
 reject_null_hypothesis_anderson(result_1.statistic, result_1.critical_values, result_1.significance_level)
 
 result_2 = anderson(all_values_2)
-print(f"Anderson-Darling test statistic 2: {result_2.statistic}, critical values: {result_2.critical_values}, significance level: {result_2.significance_level}")
+print(f"\nAnderson-Darling test statistic 2: {result_2.statistic}, critical values: {result_2.critical_values}, significance level: {result_2.significance_level}")
 reject_null_hypothesis_anderson(result_2.statistic, result_2.critical_values, result_2.significance_level)
 
 result_3 = anderson(all_values_3)
-print(f"Anderson-Darling test statistic 3: {result_3.statistic}, critical values: {result_3.critical_values}, significance level: {result_3.significance_level}")
+print(f"\nAnderson-Darling test statistic 3: {result_3.statistic}, critical values: {result_3.critical_values}, significance level: {result_3.significance_level}")
 reject_null_hypothesis_anderson(result_3.statistic, result_3.critical_values, result_3.significance_level)
 
-print(f"Shapiro-Wilk test statistic: {stat_1}, p-value: {p_value_1}")
-print(f"Shapiro-Wilk test statistic: {stat_2}, p-value: {p_value_2}")
-print(f"Shapiro-Wilk test statistic: {stat_3}, p-value: {p_value_3}\n")
 
 from scipy.stats import kstest
 
+# Set the significance level
+alpha = 0.05
+
+# Kolmogorov-Smirnov Tests
 result_1 = kstest(all_values_1, 'norm')
 print(f"Kolmogorov-Smirnov test statistic 1: {result_1.statistic}, p-value: {result_1.pvalue}")
+if result_1.pvalue > alpha:
+    print("Distribution 1 appears to be normal.")
+else:
+    print("Distribution 1 does not appear to be normal.")
 
 result_2 = kstest(all_values_2, 'norm')
 print(f"Kolmogorov-Smirnov test statistic 2: {result_2.statistic}, p-value: {result_2.pvalue}")
+if result_2.pvalue > alpha:
+    print("Distribution 2 appears to be normal.")
+else:
+    print("Distribution 2 does not appear to be normal.")
 
 result_3 = kstest(all_values_3, 'norm')
-print(f"Kolmogorov-Smirnov test statistic 3: {result_3.statistic}, p-value: {result_3.pvalue}\n")
+print(f"Kolmogorov-Smirnov test statistic 3: {result_3.statistic}, p-value: {result_3.pvalue}")
+if result_3.pvalue > alpha:
+    print("Distribution 3 appears to be normal.")
+else:
+    print("Distribution 3 does not appear to be normal.")
+print("\n")
 
+# Lilliefors Tests
 result_1, p_value_1 = lilliefors(all_values_1)
-print(f"Lilliefors test statistic: {result_1}, p-value: {p_value_1}")
+print(f"Lilliefors test statistic 1: {result_1}, p-value: {p_value_1}")
+if p_value_1 > alpha:
+    print("Distribution 1 appears to be normal.")
+else:
+    print("Distribution 1 does not appear to be normal.")
 
 result_2, p_value_2 = lilliefors(all_values_2)
-print(f"Lilliefors test statistic: {result_2}, p-value: {p_value_2}")
+print(f"Lilliefors test statistic 2: {result_2}, p-value: {p_value_2}")
+if p_value_2 > alpha:
+    print("Distribution 2 appears to be normal.")
+else:
+    print("Distribution 2 does not appear to be normal.")
 
 result_3, p_value_3 = lilliefors(all_values_3)
-print(f"Lilliefors test statistic: {result_3}, p-value: {p_value_3}\n")
-
-
-
-'''significance_level = 0.05
-if p_value_1 > significance_level:
-    print("Globalmente, i dati sembrano seguire una distribuzione normale (MONK1).")
+print(f"Lilliefors test statistic 3: {result_3}, p-value: {p_value_3}")
+if p_value_3 > alpha:
+    print("Distribution 3 appears to be normal.")
 else:
-    print("Globalmente, i dati non sembrano seguire una distribuzione normale (MONK1).")
+    print("Distribution 3 does not appear to be normal.")
+print("\n")
 
-if p_value_2 > significance_level:
-    print("Globalmente, i dati sembrano seguire una distribuzione normale (MONK2).")
-else:
-    print("Globalmente, i dati non sembrano seguire una distribuzione normale (MONK2).")
 
-if p_value_3 > significance_level:
-    print("Globalmente, i dati sembrano seguire una distribuzione normale (MONK3).")
-else:
-    print("Globalmente, i dati non sembrano seguire una distribuzione normale (MONK3).")'''
 
 
 
